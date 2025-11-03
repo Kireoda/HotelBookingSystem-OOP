4b900ef5a5151839557474ef45a327fd7cfada89
kireoda <anastasijabernsteine@gmail.com>
2025-11-03 12:02:28 +0000

    added constructors, getters and setters

---
+++ Hotel.java
@@ -0,0 +1,108 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+class Hotel {
+    private String country;
+    private String hotelName;
+    private int beds;
+    private double pricePerDay;
+    private boolean isBooked;
+    private LocalDate startBookingDate;
+    private LocalDate endBookingDate;
+    private LocalDateTime checkInTime;
+
+    //region Constructors
+    public Hotel(String country, String hotelName, int beds, double pricePerDay,
+                 boolean isBooked, LocalDate startBookingDate, LocalDate endBookingDate,
+                 LocalDateTime checkInTime) {
+        this.country = country;
+        this.hotelName = hotelName;
+        this.beds = beds;
+        this.pricePerDay = pricePerDay;
+        this.isBooked = isBooked;
+        this.startBookingDate = startBookingDate;
+        this.endBookingDate = endBookingDate;
+        this.checkInTime = checkInTime;
+    }
+
+    public Hotel() {
+        this.country = "";
+        this.hotelName = "";
+        this.beds = 0;
+        this.pricePerDay = 0.0;
+        this.isBooked = false;
+        this.startBookingDate = LocalDate.now();
+        this.endBookingDate = LocalDate.now().plusDays(7);
+        this.checkInTime = LocalDateTime.now();
+    }
+    //endregion
+
+    //region Getters and Setters
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public void setBeds(int beds) {
+        this.beds = beds;
+    }
+
+    public double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public void setBooked(boolean booked) {
+        isBooked = booked;
+    }
+
+    public LocalDate getStartBookingDate() {
+        return startBookingDate;
+    }
+
+    public void setStartBookingDate(LocalDate startBookingDate) {
+        this.startBookingDate = startBookingDate;
+    }
+
+    public LocalDate getEndBookingDate() {
+        return endBookingDate;
+    }
+
+    public void setEndBookingDate(LocalDate endBookingDate) {
+        this.endBookingDate = endBookingDate;
+    }
+
+    public LocalDateTime getCheckInTime() {
+        return checkInTime;
+    }
+
+    public void setCheckInTime(LocalDateTime checkInTime) {
+        this.checkInTime = checkInTime;
+    }
+//endregion
+
+
+}
+}
