e3c531ae2cfe3be775317182d3c0b8d51c03261e
kireoda <anastasijabernsteine@gmail.com>
2025-11-04 08:53:54 +0000

    changed start date and end date to arrival and departure

--- Reservation.java
+++ Reservation.java
@@ -2,29 +2,29 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 
-class Hotel {
+class Reservation {
     private String country;
     private String hotelName;
     private int beds;
     private double pricePerDay;
     private boolean isBooked;
-    private LocalDate startBookingDate;
-    private LocalDate endBookingDate;
+    private LocalDate arrivalDate;
+    private LocalDate departureDate;
     private LocalDateTime checkInTime;
 
     // figure out how to input csv file into constructor
 
     //region Constructors
     public Hotel(String country, String hotelName, int beds, double pricePerDay,
-                 boolean isBooked, LocalDate startBookingDate, LocalDate endBookingDate,
+                 boolean isBooked, LocalDate arrivalDate, LocalDate departureDate,
                  LocalDateTime checkInTime) {
         this.country = country;
         this.hotelName = hotelName;
         this.beds = beds;
         this.pricePerDay = pricePerDay;
         this.isBooked = isBooked;
-        this.startBookingDate = startBookingDate;
-        this.endBookingDate = endBookingDate;
+        this.arrivalDate = arrivalDate;
+        this.departureDate = departureDate;
         this.checkInTime = checkInTime;
     }
 
@@ -34,8 +34,8 @@ class Hotel {
         this.beds = 0;
         this.pricePerDay = 0.0;
         this.isBooked = false;
-        this.startBookingDate = LocalDate.now();
-        this.endBookingDate = LocalDate.now().plusDays(7);
+        this.arrivalDate = LocalDate.now();
+        this.departureDate = LocalDate.now().plusDays(7);
         this.checkInTime = LocalDateTime.now();
     }
     //endregion
@@ -81,20 +81,20 @@ class Hotel {
         isBooked = booked;
     }
 
-    public LocalDate getStartBookingDate() {
-        return startBookingDate;
+    public LocalDate getarrivalDate() {
+        return arrivalDate;
     }
 
-    public void setStartBookingDate(LocalDate startBookingDate) {
-        this.startBookingDate = startBookingDate;
+    public void setarrivalDate(LocalDate arrivalDate) {
+        this.arrivalDate = arrivalDate;
     }
 
-    public LocalDate getEndBookingDate() {
-        return endBookingDate;
+    public LocalDate getdepartureDate() {
+        return departureDate;
     }
 
-    public void setEndBookingDate(LocalDate endBookingDate) {
-        this.endBookingDate = endBookingDate;
+    public void setdepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
     }
 
     public LocalDateTime getCheckInTime() {
@@ -113,15 +113,15 @@ class Hotel {
                 ", beds=" + beds +
                 ", pricePerDay=" + pricePerDay +
                 ", isBooked=" + isBooked +
-                ", startBookingDate=" + startBookingDate +
-                ", endBookingDate=" + endBookingDate +
+                ", arrivalDate=" + arrivalDate +
+                ", departureDate=" + departureDate +
                 ", checkInTime=" + checkInTime +
                 '}';
     }
 
     //region Methods
     public int calculateDaysBooked() {
-        return (int) this.endBookingDate.toEpochDay() - (int) this.startBookingDate.toEpochDay(); // toEpochDay() was aied
+        return (int) this.departureDate.toEpochDay() - (int) this.arrivalDate.toEpochDay(); // toEpochDay() was aied
     }
 
     public double calculateTotalPrice() {
